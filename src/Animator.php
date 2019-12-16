5574ea9edd904b24d2b6f8cc72e355d8bf184b8a
Ian Rodrigues <ianrdgs@gmail.com>
2019-12-16 16:44:22 -0300

    fix minor bug

--- src/Animator.php
+++ src/Animator.php
@@ -22,20 +22,14 @@ final class Animator
 
     public function animate(int $speed, string $chamber): array
     {
-        if ($speed < 1 || $speed > 10) {
-            throw new InvalidSpeedException('Speed should be between 1 and 10.');
-        }
-
         $chamberSize = strlen($chamber);
-        if ($chamberSize < 1 || $chamberSize > 50) {
-            throw new InvalidChamberSizeException('Chamber size should be between 1 and 50.');
-        }
+        $this->assertValidation($speed, $chamberSize);
 
         $particles = $this->particleFactory->createParticles($chamber);
 
         $chamberOverTime = [];
-
         $time = 0;
+
         do {
             $rate = $time * $speed;
             $newState = $this->chamberStateFor($chamberSize, $particles, $rate);
@@ -45,6 +39,17 @@ final class Animator
         } while (trim($newState, Animator::EMPTY_LOCATION_TOKEN));
         
         return $chamberOverTime;
+    }
+
+    private function assertValidation(int $speed, int $chamberSize): void
+    {
+        if ($speed < 1 || $speed > 10) {
+            throw new InvalidSpeedException('Speed should be between 1 and 10.');
+        }
+
+        if ($chamberSize < 1 || $chamberSize > 50) {
+            throw new InvalidChamberSizeException('Chamber size should be between 1 and 50.');
+        }
     }
 
     private function chamberStateFor($chamberSize, array $particles, int $rate): string
